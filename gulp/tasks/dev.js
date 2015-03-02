f46f704269437fe7388c7ff42530e8a95df5d419
linkstrifer <linkstrifer@outlook.com>
2015-03-02 18:37:50 -0500

    [update] add icons and superscrollorama

--- gulp/tasks/dev.js
+++ gulp/tasks/dev.js
@@ -4,10 +4,10 @@ var paths = require('../config.js').paths;
 gulp.task('dev', [
 		'browser-sync:dev',
 		'stylus',
-		'unused-css'
+		// 'unused-css'
 	], function() {
-		gulp.watch(paths.base + '**/*.scss', ['sass']);
-		gulp.watch(paths.base + '**/*.styl', ['stylus']);
+		// gulp.watch(paths.base + '**/*.scss', ['sass']);
+		gulp.watch(paths.stylus + '**/*.styl', ['stylus']);
 		gulp.watch(paths.base + '**/*.html', ['html']);
-		gulp.watch(paths.base + '**/*.js', ['js']);
+		//gulp.watch(paths.base + '**/*.js', ['js']);
 });
